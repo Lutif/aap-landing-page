aff8c7e9ff779947171dd8c2a7757e7d2b4d8a0e
Lutif <lmandhro@gmail.com>
2019-12-30 14:16:24 +0500

    add faq section and bottom bar

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 
-document.querySelectorAll('.feature-tab').forEach(item => {
-    item.addEventListener('click', event => {
-        let active = document.querySelectorAll('.active').forEach(item => {
-            item.classList.remove('active');
+document.querySelectorAll('.feature-tab').forEach(item => { //loop over each feature tab
+        item.addEventListener('click', event => { // add event listener
+        let active = document.querySelectorAll('.active').forEach(item => { //find already active elements
+            item.classList.remove('active');                                //de-active them    
         });
-        if (active) { active.classList.remove('active'); }
-        let activeClass = event.target.classList[0];
-        document.querySelectorAll('.'+activeClass)[1].classList.add('active');
-        console.log(document.querySelectorAll('.'+activeClass));
+        event.target.classList.add('active'); //add active div to tab
+        let tabname = event.target.classList[0]; //find which tab is slected
+        document.querySelectorAll('.'+tabname)[1].classList.add('active'); // find details div for that tab
+        console.log(document.querySelectorAll('.'+activeClass)); //add active calss to details dive as well
         console.log(event.target.classList[0])
-        event.target.classList.add('active');
+        
     })
 })
 
+document.querySelectorAll('.question').forEach(question => {
+    question.addEventListener('click', (event) => {
+        event.target.classList.toggle('aactive');
+        let active = document.querySelectorAll('answer').forEach( ans => {
+            ans.classList.remove('aactive')
+        }
+        );
+    console.log(question.children[0].classList);
+    
+        question.children[0].classList.toggle('aactive')
+    })
+})
